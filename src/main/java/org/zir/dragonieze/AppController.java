2b2e4f243f31d52c8f59d5798eba2a56bebaf699
zirtoshka <367222@edu.itmo.ru>
2024-10-19 23:38:00 +0300

    hello

---
+++ src/main/java/org/zir/dragonieze/AppController.java
@@ -0,0 +1,25 @@
+package org.zir.dragonieze;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/dragon-controller")
+@CrossOrigin(origins = "*")
+@RequiredArgsConstructor
+public class AppController {
+
+    @GetMapping
+    public ResponseEntity<String> sayHello() {
+        final HttpHeaders httpHeaders = new HttpHeaders();
+        System.out.println("it's method sayHello");
+        return new ResponseEntity<>("{\"message\": \"Hello from secured endpoint\"}", httpHeaders, HttpStatus.OK);
+    }
+
+}
